117e6f2c8c5eb540a338ee74e2091d40efd39b9e
kirankumar <kirankumar@192.168.0.103>
2019-07-18 01:56:54 +0530

    test cases

--- src/test/java/com/myParking/AppTest.java
+++ src/test/java/com/myParking/AppTest.java
@@ -1,38 +1,134 @@
 package com.myParking;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.myParking.model.Vehicle;
+import com.myParking.service.ParkingService;
+import com.myParking.serviceImpl.ParkingServiceImpl;
+import com.myParking.util.Constants;
+
+public class AppTest {
+
+	static ParkingService parkingService;
+	
+	@BeforeClass
+	public static void setup() {
+		parkingService = new ParkingServiceImpl();
+	}
+	
+	@Test
+	public void testParkLeave() {
+		parkingService.createParkingLot(15);
+		
+		 for(int c = 1; c <= 15; c++)
+	        {
+	            try
+	            {
+	            	int slotId = parkingService.park(new Vehicle("AA-10-BB-" + c, "White"));
+	            	System.out.println("Allocated slot number:" + slotId);
+	            } catch (Exception e)
+	            {
+	                e.printStackTrace();
+	            }
+	        }
+		 
+		 parkingService.park(new Vehicle("CC-10-DD-" + 11, "White"));
+		 
+		 //Leave at 1,3,5 slots
+		 parkingService.leave(5);
+		 parkingService.leave(1);
+		 parkingService.leave(3);
+
+	}
+	
+	@Test
+    public void testGetVehiclesByColor()
     {
-        super( testName );
+		parkingService = new ParkingServiceImpl();
+        parkingService.createParkingLot(15);
+
+        for(int c = 1; c <= 10; c++)
+        {
+            try
+            {
+            	int slotId = parkingService.park(new Vehicle("EE-10-FF-" + c, "White"));
+            	System.out.println("Allocated slot number:" + slotId);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+            }
+        }
+        for(int c = 11; c <= 15; c++)
+        {
+            try
+            {
+            	int slotId = parkingService.park(new Vehicle("GG-10-HH-" + c, "Black"));
+                System.out.println("Allocated slot number:" + slotId);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+            }
+        }
+        parkingService.getDetials("White",Constants.registerColor.getConstant());
+         
+        parkingService.getDetials("Black",Constants.registerColor.getConstant());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+	@Test
+    public void testGetSlotForVehicle()
     {
-        return new TestSuite( AppTest.class );
+		parkingService = new ParkingServiceImpl();
+        parkingService.createParkingLot(12);
+
+        for(int c = 1; c <= 12; c++)
+        {
+            try
+            {
+            	int slotId =  parkingService.park(new Vehicle("KA-10-ME-" + c, "White"));
+            	System.out.println("Allocated slot number:" + slotId);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+            }
+        }
+
+         parkingService.getDetials("KA-10-ME-1",Constants.slotRegister.getConstant());
+         parkingService.getDetials("KA-10-ME-5",Constants.slotRegister.getConstant());
+         parkingService.getDetials("KA-10-ME-10",Constants.slotRegister.getConstant());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void testGetSlots()
     {
-        assertTrue( true );
+    	parkingService = new ParkingServiceImpl();
+        parkingService.createParkingLot(5);
+
+        for(int c = 1; c <= 5; c++)
+        {
+            try
+            {
+                parkingService.park(new Vehicle("KA-10-ME-" + c, "White"));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+            }
+        }
+       parkingService.getDetials("White",Constants.slotColor.getConstant());
+         
     }
+
+	
 }
--- src/test/java/com/myParking/serviceImpl/TestParkingServiceImpl.java
+++ src/test/java/com/myParking/serviceImpl/TestParkingServiceImpl.java
@@ -1,51 +1,91 @@
 package com.myParking.serviceImpl;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.myParking.model.Vehicle;
+import com.myParking.service.ParkingService;
+import com.myParking.util.Constants;
 
 public class TestParkingServiceImpl {
 
-	 
-	@Test
-	public void testParkingServiceImpl() {
-		fail("Not yet implemented");
+	static ParkingService parkingService;
+	int numSlots = 6;
+	int areaId = 4;
+	String registrationNumber ="KA01HH3141";
+	String color = "White";
+
+	@BeforeClass
+	public static void setup() {
+		parkingService = new ParkingServiceImpl();
 	}
 
 	@Test
 	public void testCreateParkingLot() {
-		
-		
-		fail("Not yet implemented");
+		parkingService.createParkingLot(numSlots);
+
 	}
 
 	@Test
 	public void testPark() {
-		fail("Not yet implemented");
+		testCreateParkingLot();
+		int slotId = parkingService.park(new Vehicle("KA-01-HH-1234", "White"));
+		assertEquals(1, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
+		slotId = parkingService.park(new Vehicle("KA-01-HH-9999", "White"));
+		assertEquals(2, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
+		slotId = parkingService.park(new Vehicle("KA-01-BB-0001", "Black"));
+		assertEquals(3, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
+		slotId = parkingService.park(new Vehicle("KA-01-HH-7777", "Red"));
+		assertEquals(4, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
+		slotId = parkingService.park(new Vehicle("KA-01-HH-2701", "Blue"));
+		assertEquals(5, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
+		slotId = parkingService.park(new Vehicle("KA-01-HH-3141", "Black"));
+		assertEquals(6, slotId);
+		System.out.println("Allocated slot number:" + slotId);
+
 	}
 
 	@Test
 	public void testLeave() {
-		fail("Not yet implemented");
+		testCreateParkingLot();
+		testPark();
+		parkingService.leave(areaId);
 	}
 
 	@Test
 	public void testGetParkinglotStatus() {
-		fail("Not yet implemented");
+		testCreateParkingLot();
+		testPark();
+		testLeave();
+		
+		parkingService.getParkinglotStatus();
 	}
-
+	
 	@Test
-	public void testGetSlots() {
-		fail("Not yet implemented");
-	}
+	public void getDetials() {
+		testCreateParkingLot();
+		testPark();
+		testLeave();
+		testGetParkinglotStatus();
+		
+		parkingService.getDetials(color, Constants.registerColor.getConstant());
+		
+		parkingService.getDetials(color, Constants.slotColor.getConstant());
+		
+		parkingService.getDetials(registrationNumber, Constants.slotRegister.getConstant());
 
-	@Test
-	public void testGetVehicleByColor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSlotOfVehicle() {
-		fail("Not yet implemented");
 	}
 
 }
